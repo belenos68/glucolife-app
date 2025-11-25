380f3df870ea86d1e329e892d6ee46bf3be962c5
belenos68 <belenos.abryelos@gmail.com>
2025-11-25 11:30:58 +0200

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
-  // Récupère toutes les variables d’environnement (Vercel + .env)
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
     plugins: [react()],
     define: {
-      // Injection explicite dans le bundle
-      'process.env': env,
-    },
-  })
+      'import.meta.env': env,
+    }
+  });
 }
